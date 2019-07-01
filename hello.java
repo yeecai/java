1b25db0f8f7e425f36c27d6cd87d247a7746d02c
liliancai <yushunchn@gmail.com>
2019-07-01 17:35:02 +0200

    basic

---
+++ hello.java
@@ -0,0 +1,13 @@
+public class hello{
+	public static void main(String[] args){
+		System.out.print("Bend o Java gang!");
+		System.out.println("Hello Java!");
+		int x = 10;
+		while(x > 0){
+			System.out.println("Asaf is a jerk.");
+			x = x - 1;
+		}
+	}
+}
+
+
